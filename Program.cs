045d4109dfb0a47f884c8ffd3b023f9341eea770
steveljko <steveljko@proton.me>
2025-03-12 23:03:23 +0100

    install swagger for api documentation

--- Program.cs
+++ Program.cs
@@ -1,22 +1,30 @@
-using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo()
+    {
+        Version = "v1",
+        Title = "Rent A Car API",
+        Description = "The Rent A Car API enables users to browse available vehicles and make rentals."
+    });
+});
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+        options.RoutePrefix = string.Empty;
+    });
 }
 
 app.UseHttpsRedirection();
-
-app.MapGet("/", () =>
-    {
-        return Results.Ok("Hello World!");
-    })
-    .WithName("Index");
 
 app.Run();
